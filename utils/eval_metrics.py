7a0ba1013e3ca80eecb4b38b059bdf4aa547fd3a
Fabio Gebhard <fabiogebhard@gmail.com>
2021-08-16 11:38:25 +0200

    create eval_metrics.py and create interfaces

---
+++ utils/eval_metrics.py
@@ -0,0 +1,30 @@
+
+# Metrics to evaluate the different trained classification models:
+# - Recall
+# - Specificity
+# - Precision
+# - F1-Score
+# - Accuracy
+
+def get_counts(truth_labels, pred_labels):
+    ...
+
+
+def calc_recall():
+    ...
+
+
+def calc_specificity():
+    ...
+
+
+def calc_precision():
+    ...
+
+
+def calc_f1score():
+    ...
+
+
+def calc_accuracy():
+    ...
