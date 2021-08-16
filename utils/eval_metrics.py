d9c72ff2dba2377ba76a8ddfd123031b22b10067
Fabio Gebhard <fabiogebhard@gmail.com>
2021-08-16 12:40:17 +0200

    example of conf_matrix visualization

--- utils/eval_metrics.py
+++ utils/eval_metrics.py
@@ -1,4 +1,7 @@
 from sklearn.metrics import confusion_matrix, classification_report
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sn
 
 # Metrics to evaluate the different trained multi-classification models:
 # - Recall
@@ -12,7 +15,15 @@ y_pred = [1, 2, 2, 0, 1, 2, 1]
 
 print(confusion_matrix(y_true, y_pred, labels=[0, 1, 2]))
 conf_matrix = confusion_matrix(y_true, y_pred, labels=[0, 1, 2])
-print(classification_report(y_true, y_pred, target_names=['class1', 'class2', 'class3']))
-report = classification_report(y_true, y_pred, target_names=['class1', 'class2', 'class3'])
-print('x')
 
+# visualization of confusion matrix
+df_cm = pd.DataFrame(conf_matrix, index=[i for i in ['NORMAL', 'COVID-19', 'PNEUMONIA']],
+                     columns=[i for i in ['NORMAL', 'COVID-19', 'PNEUMONIA']])
+plt.figure(figsize=(10, 7))
+ax = sn.heatmap(df_cm, annot=True)
+plt.show()
+
+
+print(classification_report(y_true, y_pred, target_names=['NORMAL', 'COVID-19', 'PNEUMONIA']))
+report = classification_report(y_true, y_pred, target_names=['NORMAL', 'COVID-19', 'PNEUMONIA'])
+print('x')
