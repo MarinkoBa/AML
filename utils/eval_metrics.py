0d2ac53eb7f42569d0f2b1f54eb2f475484e7b9e
Fabio Gebhard <fabiogebhard@gmail.com>
2021-09-22 12:52:01 +0200

    create methods for evaluation metrics and confusion matrix

--- utils/eval_metrics.py
+++ utils/eval_metrics.py
@@ -2,6 +2,8 @@ from sklearn.metrics import confusion_matrix, classification_report
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sn
+import os
+
 
 # Metrics to evaluate the different trained multi-classification models:
 # - Recall
@@ -10,22 +12,58 @@ import seaborn as sn
 # - F1-Score
 # - Accuracy
 
-y_true = [1, 1, 2, 0, 1, 0, 2]
-y_pred = [1, 2, 2, 0, 1, 2, 1]
+def get_confusion_matrix(y_pred, y_truth, filename, show_plt=False):
+    """
+        Parameters
+        ----------
+        y_pred : array
+            Contains predicted labels by the model
+        y_truth : array
+            Contains the ground truth labels
+        filename : str
+            Name of the created png-file
+        show_plt : bool
+            Marks if the plot of the confusion matrix should be shown during run time
 
-print(confusion_matrix(y_true, y_pred, labels=[0, 1, 2]))
-conf_matrix = confusion_matrix(y_true, y_pred, labels=[0, 1, 2])
+    """
 
-# TODO - method saves as png, visualization of confusion matrix
-df_cm = pd.DataFrame(conf_matrix, index=[i for i in ['NORMAL', 'COVID-19', 'PNEUMONIA']],
-                     columns=[i for i in ['NORMAL', 'COVID-19', 'PNEUMONIA']])
-plt.figure(figsize=(10, 7))
+    print(confusion_matrix(y_truth, y_pred, labels=[0, 1, 2]))
+    conf_matrix = confusion_matrix(y_truth, y_pred, labels=[0, 1, 2])
 
-ax = sn.heatmap(df_cm, annot=True, cbar=False, linewidths=.5, cmap='mako')
-plt.show()
+    df_cm = pd.DataFrame(conf_matrix, index=[i for i in ['NORMAL', 'COVID-19', 'PNEUMONIA']],
+                         columns=[i for i in ['NORMAL', 'COVID-19', 'PNEUMONIA']])
+    plt.figure(figsize=(10, 7))
 
-# TODO - implement as method, save as text-file, which can be read later -> method
-#
-print(classification_report(y_true, y_pred, target_names=['NORMAL', 'COVID-19', 'PNEUMONIA']))
-report = classification_report(y_true, y_pred, target_names=['NORMAL', 'COVID-19', 'PNEUMONIA'])
-print('x')
+    ax = sn.heatmap(df_cm, annot=True, cbar=False, linewidths=.5, cmap='mako')
+
+    if show_plt is True:
+        plt.show()
+
+    # save plt as png in log-folder
+    path = os.getcwd()
+    parent = os.path.dirname(path)
+
+    plt.savefig(parent + '/log/' + filename)
+
+
+def get_evaluation_metrics(y_pred, y_truth, filename):
+    """
+        Parameters
+        ----------
+        y_pred : array
+            Contains predicted labels by the model
+        y_truth : array
+            Contains the ground truth labels
+        filename : str
+            Name of the created file, which contains the evaluation metrics
+    """
+
+    print(classification_report(y_truth, y_pred, target_names=['NORMAL', 'COVID-19', 'PNEUMONIA']))
+    report = classification_report(y_truth, y_pred, target_names=['NORMAL', 'COVID-19', 'PNEUMONIA'], output_dict=True)
+    df = pd.DataFrame(report).transpose()
+
+    # save result as csv file in log folder
+    path = os.getcwd()
+    parent = os.path.dirname(path)
+
+    df.to_csv(parent + '/log/' + filename)
